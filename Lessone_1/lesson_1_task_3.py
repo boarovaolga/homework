2a1c311fef7c64f9152c3a6bd8af4a9fa0abc38a
boarovaolga <boarovaolga23@gmail.com>
2026-01-22 09:43:46 +0300

    Исправление

--- Lessone_1/lesson_1_task_3.py
+++ Lessone_1/lesson_1_task_3.py
@@ -1,3 +1,3 @@
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
-print("Вас зовут:" + first_name + last_name)
+print("Вас зовут:" + first_name + " " + last_name)
